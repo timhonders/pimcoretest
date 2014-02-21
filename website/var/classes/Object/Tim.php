fe96959fbc0d79468f5091a9b93fb501afaf42e1
root <root@tim.tim>
2014-02-21 15:57:07 +0100

    x

---
+++ website/var/classes/Object/Tim.php
@@ -0,0 +1,65 @@
+<?php 
+
+class Object_Tim extends Object_Concrete {
+
+public $o_classId = 2;
+public $o_className = "tim";
+public $test;
+public $ddd;
+
+
+/**
+* @param array $values
+* @return Object_Tim
+*/
+public static function create($values = array()) {
+	$object = new self();
+	$object->setValues($values);
+	return $object;
+}
+
+/**
+* @return string
+*/
+public function getTest () {
+	$preValue = $this->preGetValue("test"); 
+	if($preValue !== null && !Pimcore::inAdmin()) { return $preValue;}
+	$data = $this->test;
+	 return $data;
+}
+
+/**
+* @param string $test
+* @return void
+*/
+public function setTest ($test) {
+	$this->test = $test;
+	return $this;
+}
+
+/**
+* @return string
+*/
+public function getDdd () {
+	$preValue = $this->preGetValue("ddd"); 
+	if($preValue !== null && !Pimcore::inAdmin()) { return $preValue;}
+	$data = $this->ddd;
+	 return $data;
+}
+
+/**
+* @param string $ddd
+* @return void
+*/
+public function setDdd ($ddd) {
+	$this->ddd = $ddd;
+	return $this;
+}
+
+protected static $_relationFields = array (
+);
+
+public $lazyLoadedFields = NULL;
+
+}
+
---
+++ website/var/classes/Object/Tim/List.php
@@ -0,0 +1,9 @@
+<?php 
+
+class Object_Tim_List extends Object_List_Concrete {
+
+public $classId = 2;
+public $className = "tim";
+
+
+}
