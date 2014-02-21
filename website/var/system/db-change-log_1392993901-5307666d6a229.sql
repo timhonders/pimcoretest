8b5b3aa503a6ac3466f051c0c2c2e55de7e18dcc
root <root@tim.tim>
2014-02-21 15:45:32 +0100

    remove object

---
+++ website/var/system/db-change-log_1392993901-5307666d6a229.sql
@@ -0,0 +1,28 @@
+DELETE FROM `classes` WHERE (id = 1);
+
+/*--NEXT--*/
+
+DROP TABLE `object_query_1`;
+
+/*--NEXT--*/
+
+DROP TABLE `object_store_1`;
+
+/*--NEXT--*/
+
+DROP TABLE `object_relations_1`;
+
+/*--NEXT--*/
+
+DROP TABLE IF EXISTS `object_metadata_1`;
+
+/*--NEXT--*/
+
+DROP VIEW `object_1`;
+
+/*--NEXT--*/
+
+DROP TABLE IF EXISTS object_localized_data_1;
+
+/*--NEXT--*/
+
